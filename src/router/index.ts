541303a01b8c264f4d36db348b1c7fdbd91570e7
2894462606 <2894462606@qq.com>
2024-04-20 00:01:39 +0800

    feat(页面): 关于页面开发
    About.vue;
    路由添加;

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import Home from "@/views/Home/HomeView.vue";
+import HomeView from "@/views/Home/HomeView.vue";
+import FarmView from "@/views/Farm/FarmView.vue";
+import ShopView from "@/views/Shop/ShopView.vue";
+import CommunityView from "@/views/Community/CommunityView.vue";
+import UserView from "@/views/User/UserView.vue";
+import PageLayout from "@/layouts/PageLayout.vue";
+import AboutView from "@/views/About/AboutView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "", redirect: "/home" },
@@ -8,10 +15,26 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     component: BasicLayout,
     children: [
-      { path: "home", component: Home, meta: { title: "首页" } },
+      { path: "home", component: HomeView, meta: { title: "首页" } },
+      { path: "farm", component: FarmView, meta: { title: "农场中心" } },
+      { path: "shop", component: ShopView, meta: { title: "云闽商城" } },
+      {
+        path: "community",
+        component: CommunityView,
+        meta: { title: "社区交流" },
+      },
+      { path: "user", component: UserView, meta: { title: "个人中心" } },
       { path: "", redirect: "/" },
     ],
   },
+  {
+    path: "/",
+    component: PageLayout,
+    children: [
+      { path: "/about", component: AboutView, meta: { title: "关于" } },
+      { path: "", redirect: "/" ,
+    ,
+  ,
 ];
 
 const router = createRouter({
---
+++ src/views/About/AboutView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import beian from "@/assets/beian.png";
+</script>
+
+<template>
+  <a
+    href="https://beian.mps.gov.cn/#/query/webSearch?code=35080202351360"
+    class="text-gray-500 text-lg flex items-center justify-center"
+    rel="noreferrer"
+    target="_blank"
+  >
+    <van-image :src="beian" class="w-4 h-4 mr-1" />
+    <p>闽公网安备35080202351360</p>
+  </a>
+</template>
+
+<style scoped></style>
