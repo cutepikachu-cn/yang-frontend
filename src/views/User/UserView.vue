5c482bc8ddaf5adad930522c7eae2a00ac74dba7
2894462606 <2894462606@qq.com>
2024-04-19 23:59:02 +0800

    feat(页面): 个人中心页面开发
    User.vue;

---
+++ src/views/User/UserView.vue
@@ -0,0 +1,147 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const userInfo = ref({
+  id: 1,
+  userNickname: "笨蛋皮卡丘",
+  userAvatar: "http://source.cute-pikachu.cn/img/avatar.webp",
+  userRole: "farm",
+});
+
+onMounted(async () => {
+  return;
+});
+
+const active = ref(0);
+
+const actions = [{ text: "关于" }];
+
+const onSelect = (
+  action: {
+    text: string;
+    icon: string;
+    color: string;
+    disabled: boolean;
+    className: string | Array<never> | object;
+  },
+  index: number
+) => {
+  console.log(action);
+  if (index === 0) {
+    router.push("/about");
+  }
+};
+</script>
+
+<template>
+  <div class="title rounded-b-3xl bg-primary shadow-primary shadow-md h-24 p-1">
+    <div class="menu absolute text-2xl right-2">
+      <van-popover :actions="actions" placement="bottom-end" @select="onSelect">
+        <template #reference>
+          <van-icon class-prefix="fa-duotone fa-bars" color="white" />
+        </template>
+      </van-popover>
+    </div>
+    <h1
+      class="text-2xl text-center text-white"
+      v-if="userInfo?.userRole === 'farm'"
+    >
+      羊场主中心
+    </h1>
+    <h1 class="text-2xl text-center text-white" v-else>小金主个人中心</h1>
+  </div>
+
+  <div
+    class="info bg-white h-50 shadow-[0_-5px_20px_0_white,0_5px_5px_0_rgb(25,137,250,0.5)] mx-3 p-2 rounded-3xl ring-white flex flex-col items-center relative"
+  >
+    <van-image
+      round
+      fit="cover"
+      class="-top-10 absolute w-20 h-20 shadow-[0_-5px_20px_0_white,0_5px_5px_0_rgb(25,137,250,0.5)]"
+      :src="userInfo?.userAvatar"
+    />
+    <div class="mt-11 mb-1 text-xl text-black -top-10 flex items-center h-10">
+      <h2 class="mr-2">{{ userInfo?.userNickname }}</h2>
+      <van-tag type="primary" class="px-3" size="large"> TAG</van-tag>
+    </div>
+
+    <div class="func w-full">
+      <van-row align="center" justify="space-around" :gutter="[10, 10]" wrap>
+        <van-col span="12">
+          <van-button
+            type="primary"
+            round
+            plain
+            size="large"
+            text="用户信息"
+            class="w-full shadow-md shadow-primary/20"
+          />
+        </van-col>
+        <van-col span="12">
+          <van-button
+            type="primary"
+            round
+            plain
+            size="large"
+            text="订单管理"
+            class="w-full shadow-md shadow-primary/20"
+          />
+        </van-col>
+        <van-col span="12">
+          <van-button
+            type="primary"
+            round
+            plain
+            size="large"
+            text="商品管理"
+            class="w-full shadow-md shadow-primary/20"
+            v-if="userInfo?.userRole === 'farm'"
+          />
+        </van-col>
+        <van-col span="12">
+          <van-button
+            type="primary"
+            round
+            plain
+            size="large"
+            text="推广管理"
+            class="w-full shadow-md shadow-primary/20"
+            v-if="userInfo?.userRole === 'farm'"
+          />
+        </van-col>
+      </van-row>
+    </div>
+  </div>
+
+  <div class="more my-4">
+    <van-tabs v-model:active="active" swipeable>
+      <van-tab title-class="text-lg">
+        <template #title>
+          <van-badge dot color="#facc15"> 待参加</van-badge>
+        </template>
+        待参加
+      </van-tab>
+      <van-tab title-class="text-lg">
+        <template #title>
+          <van-badge dot color="#facc15"> 待签收</van-badge>
+        </template>
+        待签收
+      </van-tab>
+      <van-tab title-class="text-lg">
+        <template #title>
+          <van-badge dot color="#facc15"> 售后</van-badge>
+        </template>
+        售后
+      </van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<style scoped>
+:deep(.van-tab) .van-tab__text {
+  overflow: visible;
+}
+</style>
