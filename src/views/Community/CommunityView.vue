d5a2be3de712f00dd4f1f3be0ff4c28f3791b291
2894462606 <2894462606@qq.com>
2024-04-19 23:58:19 +0800

    feat(页面): 社区交流页面开发
    CommunityView.vue;

---
+++ src/views/Community/CommunityView.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import GroupItem from "./components/GroupItem.vue";
+
+const active = ref(0);
+
+const loading = ref(false);
+const finished = ref(false);
+
+const groups = ref<
+  {
+    title: string;
+    avatar: string;
+  }[]
+>([]);
+
+const onLoad = () => {
+  groups.value = [
+    {
+      title: "厦门外地乡贤群",
+      avatar: "http://source.cute-pikachu.cn/img/default.webp",
+    },
+    {
+      title: "泉州外地乡贤群",
+      avatar: "http://source.cute-pikachu.cn/img/default.webp",
+    },
+    {
+      title: "泉州外地团购群",
+      avatar: "http://source.cute-pikachu.cn/img/default.webp",
+    },
+  ];
+  loading.value = false;
+};
+</script>
+
+<template>
+  <van-row justify="space-around" align="center" class="mx-0.5 my-2">
+    <van-col span="6" v-for="i in 4" :key="i" class="text-center">
+      <van-image
+        round
+        fit="cover"
+        class="shadow-md shadow-primary/50 size-14"
+        src="http://source.cute-pikachu.cn/img/default.webp"
+      />
+    </van-col>
+  </van-row>
+  <van-tabs v-model:active="active" shrink>
+    <van-tab title="常用">
+      <van-cell title="课程" is-link center size="large" to="/course">
+        <template #icon>
+          <van-icon
+            class-prefix="fa-duotone fa-book fa-3x"
+            class="mr-2 text-success"
+          />
+        </template>
+      </van-cell>
+      <van-cell title="发现" is-link center size="large">
+        <template #icon>
+          <van-icon
+            class-prefix="fa-duotone fa-file-magnifying-glass fa-3x"
+            class="mr-2 text-success"
+          />
+        </template>
+      </van-cell>
+      <van-cell title="分析" is-link center size="large">
+        <template #icon>
+          <van-icon
+            class-prefix="fa-duotone fa-chart-mixed fa-3x"
+            class="mr-2 text-success"
+          />
+        </template>
+      </van-cell>
+    </van-tab>
+    <van-tab title="推荐"> 推荐</van-tab>
+  </van-tabs>
+
+  <van-row justify="space-around" align="center" class="my-2">
+    <van-col span="8">
+      <van-button
+        type="primary"
+        plain
+        text="附近场主"
+        size="large"
+        class="w-full shadow-inner shadow-primary/50"
+      />
+    </van-col>
+    <van-col span="8">
+      <van-button
+        type="primary"
+        plain
+        text="加盟我们"
+        size="large"
+        class="w-full shadow-inner shadow-primary/50"
+      />
+    </van-col>
+  </van-row>
+
+  <van-list
+    :loading="loading"
+    :finished="finished"
+    finished-text="没有更多了"
+    @load="onLoad"
+  >
+    <GroupItem
+      v-for="(group, index) in groups"
+      :key="index"
+      :title="group.title"
+      :avatar="group.avatar"
+    />
+  </van-list>
+</template>
+
+<style scoped></style>
---
+++ src/views/Community/components/GroupItem.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps<{
+  title: string;
+  avatar: string;
+}>();
+</script>
+
+<template>
+  <van-cell
+    :title="title"
+    is-link
+    center
+    :border="false"
+    :clickable="false"
+    size="large"
+    title-class="shadow-lg shadow-primary/20 rounded-lg text-center"
+  >
+    <template #icon>
+      <van-image
+        round
+        fit="cover"
+        class="shadow-md shadow-primary/50 size-10"
+        :src="avatar"
+      />
+    </template>
+  </van-cell>
+</template>
+
+<style scoped></style>
