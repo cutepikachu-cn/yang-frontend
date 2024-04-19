3d526c6c15cb37e149d8d1df7c3e0a87b884f9c1
2894462606 <2894462606@qq.com>
2024-04-19 23:06:30 +0800

    feat(页面): 添加基础布局
    BasicLayout.vue 主页（基础）面布局;
    PageLayout.vue 普通页面布局

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const active = ref(2);
+
+const tabItemList = [
+  {
+    title: "农场中心",
+    to: "/farm",
+    iconClassPrefix: "fa-duotone fa-farm",
+  },
+  {
+    title: "云闽商城",
+    to: "/shop",
+    iconClassPrefix: "fa-duotone fa-shop",
+  },
+  {
+    title: "首页",
+    to: "/home",
+    iconClassPrefix: "fa-duotone fa-house",
+  },
+  {
+    title: "社区交流",
+    to: "/community",
+    iconClassPrefix: "fa-duotone fa-messages",
+  },
+  {
+    title: "个人中心",
+    to: "/user",
+    iconClassPrefix: "fa-duotone fa-user",
+  },
+];
+</script>
+
+<template>
+  <router-view />
+  <van-tabbar v-model="active" placeholder safe-area-inset-bottom>
+    <van-tabbar-item
+      v-for="(item, index) in tabItemList"
+      :key="index"
+      :to="item.to"
+    >
+      <template #icon>
+        <van-icon :class-prefix="item.iconClassPrefix" class="text-3xl/none" />
+      </template>
+      {{ item.title }}
+    </van-tabbar-item>
+    <!--<van-tabbar-item to="/farm">-->
+    <!--  <template #icon>-->
+    <!--    <van-icon class-prefix="fa-duotone fa-farm"/>-->
+    <!--  </template>-->
+    <!--  农场中心-->
+    <!--</van-tabbar-item>-->
+    <!--<van-tabbar-item to="/shop">-->
+    <!--  <template #icon>-->
+    <!--    <van-icon class-prefix="fa-duotone fa-shop"/>-->
+    <!--  </template>-->
+    <!--  云闽商城-->
+    <!--</van-tabbar-item>-->
+    <!--<van-tabbar-item to="/home">-->
+    <!--  <template #icon>-->
+    <!--    <van-icon class-prefix="fa-duotone fa-house"/>-->
+    <!--  </template>-->
+    <!--  首页-->
+    <!--</van-tabbar-item>-->
+    <!--<van-tabbar-item to="/community">-->
+    <!--  <template #icon>-->
+    <!--    <van-icon class-prefix="fa-duotone fa-messages"/>-->
+    <!--  </template>-->
+    <!--  社区交流-->
+    <!--</van-tabbar-item>-->
+    <!--<van-tabbar-item to="/user">-->
+    <!--  <template #icon>-->
+    <!--    <van-icon class-prefix="fa-duotone fa-user"/>-->
+    <!--  </template>-->
+    <!--  个人中心-->
+    <!--</van-tabbar-item>-->
+  </van-tabbar>
+</template>
+
+<style scoped lang="scss">
+.van-tabbar__placeholder :deep(.van-tabbar) {
+  height: auto;
+  padding: 0.25rem 0;
+}
+</style>
---
+++ src/layouts/PageLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const onClickLeft = () => router.back();
+
+const route = useRoute();
+const title = route.meta.title || "Page";
+</script>
+
+<template>
+  <van-nav-bar
+    :title="title"
+    left-text="返回"
+    left-arrow
+    @click-left="onClickLeft"
+    placeholder
+    fixed
+  />
+  <router-view />
+</template>
+
+<style scoped lang="scss"></style>
