a0fbfc6fd77e87b4d727ec35c3ed8b1f488a32fd
2894462606 <2894462606@qq.com>
2024-05-01 23:24:00 +0800

    feat(业务): Tabbar状态管理优化
    引入 VueUse 依赖，使用 useStorage 替代 ref 进行 Tabbar 状态管理

--- src/store/tabbar.ts
+++ src/store/tabbar.ts
@@ -1,9 +1,9 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { useStorage } from "@vueuse/core";
 
 export const useTabbarStore = defineStore("tabbar", () => {
-  const userViewActiveTab = ref(2);
-  const farmViewActiveTab = ref(2);
+  const userViewActiveTab = useStorage("userViewActiveTab", 2);
+  const farmViewActiveTab = useStorage("farmViewActiveTab", 2);
 
   return { userViewActiveTab, farmViewActiveTab };
 });
